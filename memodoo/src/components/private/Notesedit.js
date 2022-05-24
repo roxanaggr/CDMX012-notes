85723db340e247e25e37dae5514c0b8d2f434a8d
roxanaggr <roxanaggr@gmail.com>
2022-05-24 10:38:32 -0500

    fixed minor detail to get notes async function

--- memodoo/src/components/private/Notesedit.js
+++ memodoo/src/components/private/Notesedit.js
@@ -3,11 +3,13 @@ import Navbargreen from './Navbargreen';
 import { Cancelnote } from './Cancelnote';
 import { useNavigate, useParams } from 'react-router-dom';
 import '../../App.css';
-import { db, doc, getDoc, collection, setDoc, updateDoc } from '../../lib/firestore';
+import { db, doc, getDoc, setDoc } from '../../lib/firestore';
 import { serverTimestamp } from 'firebase/firestore';
 import { auth } from '../../lib/firebase';
 
 function Notesedit() {
+
+ 
 
   const thisNote = {
     title: '',
@@ -21,10 +23,14 @@ function Notesedit() {
   let navigate = useNavigate();
   const {id} = useParams();
 
+ /*  const docRef = doc(db, 'notes', id);
+  const docSnap = getDoc(docRef);
+  console.log(docSnap.data()) */
+
   const getNotesId = async () => {
     try {
-      const collectionRef = collection(db, 'notes');
-      const docSnap = await getDoc(collectionRef, id);
+      const docRef = doc(db, 'notes',id);
+      const docSnap = await getDoc(docRef, id);
       setupdateNote(docSnap.data())
     } catch (e) {
       console.error(e);
@@ -58,14 +64,14 @@ function Notesedit() {
                 <form className='form-note' onSubmit={(e)=>{handleEditNote(e)}} >
                     <input type="text" id="title"
                     name="title"
-                    placeholder="Your note's title"
-                    value={updateNote}
+                    //placeholder="Your note's title"
+                    value={updateNote.title}
                     onChange ={(e) => {setupdateNote({...updateNote, title: e.target.value })}}>
                     </input>
                     <textarea id="note"
                     name="content"
-                    placeholder="Type your note!"
-                    value={updateNote}
+                    //placeholder="Type your note!"
+                    value={updateNote.content}
                     onChange ={(e) => {setupdateNote({...updateNote, content: e.target.value })}}>
                     </textarea>
                     <section className='actions-note'>
